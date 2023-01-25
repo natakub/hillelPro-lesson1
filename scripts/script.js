e1091ad7644c6a193bc107f33e5fa64e48d9ba38
Nataliia Kubytska <natashakub27@gmail.com>
2023-01-25 14:20:45 +0200

    added calculator with input values

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// calculator 1
 function calculate(num1, operator, num2) {
   if (operator === "+") {
     console.log(num1 + num2);
@@ -14,4 +15,33 @@ function calculate(num1, operator, num2) {
   }
 }
 
-calculate(10, "%", 100);
+calculate(30, "+", 40);
+calculate(220, "-", 40);
+calculate(30, "*", 40);
+calculate(300, "/", 10);
+calculate(10, "%", 225);
+
+// calculate inputs values
+function showInputValue() {
+  let num1 = document.getElementById("firstValue").value;
+  let num2 = document.getElementById("secondValue").value;
+  let operator = document.getElementById("operator").value;
+
+  if (operator === "+") {
+    console.log(Number(num1) + Number(num2));
+  } else if (operator === "-") {
+    console.log(num1 - num2);
+  } else if (operator === "*") {
+    console.log(num1 * num2);
+  } else if (operator === "/") {
+    console.log(num1 / num2);
+  } else if (operator === "%") {
+    console.log((num1 / 100) * num2);
+  } else {
+    console.log(
+      "Undefined operator. Please, enter one of the following: +, -, *, /, %"
+    );
+  }
+}
+
+showInputValue();
